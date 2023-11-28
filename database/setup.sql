0e5f4f1c5c8b235992372df0f574097ff5056def
VincentGonnet <vincent.gonnet08@gmail.com>
2023-11-28 15:35:39 +0100

    refactor: project structure

---
+++ database/setup.sql
@@ -0,0 +1,19 @@
+-- REMOVE OLD TABLES
+
+
+
+
+-- CREATE TABLES
+
+-- CREATE TABLE client (
+--     emailUtilisateur varchar(255) PRIMARY KEY,
+--     nomClient varchar(255) NOT NULL,
+--     FOREIGN KEY (emailUtilisateur) REFERENCES utilisateur (emailUtilisateur)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- SEED TABLES
+
+
+
