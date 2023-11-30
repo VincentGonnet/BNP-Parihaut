3ca8f4999823522e414bec9d1585bab3399408da
VincentGonnet <vincent.gonnet08@gmail.com>
2023-11-30 10:58:15 +0100

    feat: global layout setup

--- src/index.php
+++ src/index.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BNP Parishaut</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <?php
-    require_once 'view/global-layout.php'
+    require_once 'view/view.php'
         ?>
 </body>
 
--- src/view/global-layout.php
+++ src/view/global-layout.php
@@ -1,3 +1,12 @@
+<nav>
+    <?= $nav ?>
+</nav>
 <div>
 
+    <header>
+        <?= $head ?>
+    </header>
+    <main>
+        <?= $view ?>
+    </main>
 </div>
