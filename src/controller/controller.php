6938c1e5c2117508ef2bf0de4fcb9f3a34f8196c
VincentGonnet <vincent.gonnet08@gmail.com>
2023-12-06 15:50:01 +0100

    rebase

--- src/controller/controller.php
+++ src/controller/controller.php
@@ -1,7 +1,20 @@
 <?php
 require_once 'view/view.php';
+session_start();
 
-function CtlDisplayLoginPage(){
+require_once 'model/user.php';
+
+// if not logged in in the session
+if(!isset($_SESSION['loggedIn'])) {
+    $_SESSION['loggedIn'] = false;
+}
+
+function CtlLogin($username, $password) {
+    $user = logIn($username, $password);
+    // TODO: if user is null, display error message
+}
+
+function CtlDisplayLoginPage() {
     displayLoginPage();
 }
 
--- src/controller/front-controller.php
+++ src/controller/front-controller.php
@@ -2,10 +2,17 @@
 
 require_once 'controller/controller.php';
 
-if (isset($_POST['connection'])){
-    CtlGlobalLayout();
-} else{
+if(isset($_POST['login'])) {
+    // get the username and password from the form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    CtlLogin($username, $password);
+}
+
+if($_SESSION['loggedIn'] == false) {
     CtlDisplayLoginPage();
+} else {
+    CtlGlobalLayout();
 }
 
 
