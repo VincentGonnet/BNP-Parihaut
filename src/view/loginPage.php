b5e7afae4fbc5a4eebde322de99ce3294587fe84
Joan <joan.guillard@gmail.com>
2023-12-08 14:40:02 +0100

    feat: login page css

--- src/view/loginPage.php
+++ src/view/loginPage.php
@@ -3,23 +3,30 @@
     
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>BNP Parishaut - Login</title>
+        <title>BNP Parihaut - Login</title>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link rel="stylesheet" href="style.css">
     </head>
+
+    <div class="logo">
+            <img src="bnp_parihaut.jpg" alt="bnplogo" />
+    </div>
+
     
-    <body>
+    <body id="loginBody">
         <form id="loginForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <div class="center"><h2>&nbsp;Bienvenue sur BNP Parihaut&nbsp;</h2></div>
             <fieldset>
-                <legend>Connexion à votre compte</legend>
-                    <p><label>Votre login : </label></p>
-                    <p><input type="text" name="login" /></p>
-                    <p><label>Votre mot de passe : </label></p>
-                    <p><input type="text" name="password" /></p>
-                    <p><label class="label_nostyle">&nbsp;</label>
-                    <p><input type="submit" value="Se connecter" name="connection" /></p>
+                <p><label>Votre identifiant  </label></p>
+                <div class="inputBox"><input type="text" name="login" required><i class='bx bxs-user'></i></div><br>
+                <p><label>Votre mot de passe  </label></p>
+                <div class="inputBox"><input type="password" name="password" required><i class='bx bxs-lock-alt'></i></div>
+                <p><label class="label_nostyle">&nbsp;</label><div class="center"><input type="submit" value="Se connecter" name="connection" /></div></p>
             </fieldset>
         </form>
+        
 </body>
 
 </html>
