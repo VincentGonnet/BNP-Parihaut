2b8d6327d0f9cabb7ef93d095c90c23fb291f8f3
VincentGonnet <vincent.gonnet08@gmail.com>
2023-12-08 18:44:22 +0100

    fix: form not centered

--- src/view/login-page.php
+++ src/view/login-page.php
@@ -9,24 +9,32 @@
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link rel="stylesheet" href="style.css">
     </head>
-
-    <div class="logo">
-            <img src="bnp_parihaut.jpg" alt="bnplogo" />
-    </div>
-
     
     <body id="loginBody">
+        <div class="logo">
+                <img src="bnp_parihaut.jpg" alt="bnplogo" />
+        </div>
         <form id="loginForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <div class="center"><h2>&nbsp;Bienvenue sur BNP Parihaut&nbsp;</h2></div>
+            <h2>&nbsp;Bienvenue sur BNP Parihaut&nbsp;</h2>
             <fieldset>
-                <p><label>Votre identifiant  </label></p>
-                <div class="inputBox"><input type="text" name="login" required><i class='bx bxs-user'></i></div><br>
-                <p><label>Votre mot de passe  </label></p>
-                <div class="inputBox"><input type="password" name="password" required><i class='bx bxs-lock-alt'></i></div>
-                <p><label class="label_nostyle">&nbsp;</label><div class="center"><input type="submit" value="Se connecter" name="connection" /></div></p>
-            </fieldset>
-        </form>
-        
-</body>
+                
+                <label>Votre identifiant  </label>
+                <div class="inputBox">
+                    <input type="text" name="login" required>
+                    <i class='bx bxs-user'></i>
+                </div>
 
+                <label>Votre mot de passe  </label>
+                <div class="inputBox">
+                    <input type="password" name="password" required>
+                    <i class='bx bxs-lock-alt'></i>
+                </div>
+
+                <div class="center">
+                    <input type="submit" value="Se connecter" name="connection" />
+                </div>
+
+            </fieldset>
+        </form>    
+    </body>
 </html>
