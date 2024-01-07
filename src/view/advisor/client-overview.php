67f27bc46163893a3326778ef140d271f21c4e25
Joan <joan.guillard@gmail.com>
2024-01-07 20:27:30 +0100

    feat : advisor edit client

--- src/view/advisor/client-overview.php
+++ src/view/advisor/client-overview.php
@@ -42,7 +42,7 @@
 
         <p>
             <label id="adress"> Adresse:</label>
-            <input  name="input-adress" for="adress" type="text" value="<?= $adress ?>" readOnly>
+            <input  name="input-adress" class="read" for="adress" type="text" value="<?= $adress ?>" readOnly>
         </p>
 
 
@@ -55,23 +55,23 @@
     <div>
         <p> 
             <label id="mail"> Mail:</label>
-            <input  name="input-mail" for="mail" type="text" value="<?= $mail ?>" readOnly>
+            <input  name="input-mail" class="read" for="mail" type="text" value="<?= $mail ?>" readOnly>
         </p> 
 
         <p>
             <label id="phone">Numéro de telephone:</label>
-            <input name="input-phone-number"  for="phone" type="text" value="<?= $phoneNumber?>" readOnly>
+            <input name="input-phone-number" class="read" for="phone" type="text" value="<?= $phoneNumber?>" readOnly>
         </p>
 
         <p>
             <label id="situation">Situation familiale:</label>
-            <input name="input-situation"  for="situation" type="text" value="<?= $situation?>" readOnly>
+            <input name="input-situation" class="read" for="situation" type="text" value="<?= $situation?>" readOnly>
         </p>
 
 
         <p>
             <label id="work">Profession:</label>
-            <input name="input-work" for="work" type="text" value="<?= $work ?>" readOnly>
+            <input name="input-work" for="work" class="read" type="text" value="<?= $work ?>" readOnly>
         </p>
 
     
@@ -103,9 +103,33 @@
     </div>
 
     <div>
+        <button   type="button" value="Modifier" onClick= "modifier()">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+            </svg>
+            Modifier
+        </button>
+
+        <button type="submit" name="submit-overview-changes">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+            </svg>
+            Valider
+        </button>
     </div>
 
     <?php  } ?>
 
+    <script>
+    var readInputs = document.querySelectorAll('#overview .read');
 
+    function modifier() {
+        readInputs.forEach(function (input) {
+            input.readOnly = false;
+        });
+    }
+    
+    
+</script>
 </form>
+
