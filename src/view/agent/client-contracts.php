e425b367277cb8aee51c55046ebed7a480bf52a2
hamze hamze <jeannot.lapin@basse-cour.fr>
2024-01-08 17:05:11 +0100

    conrtact style fixed

--- src/view/agent/client-contracts.php
+++ src/view/agent/client-contracts.php
@@ -13,7 +13,7 @@
                 echo '<p>Aucun contrat n\'est disponible pour ce client.</p>';
             } 
         else {
-            echo '<div id="div-container">';
+            
             foreach ($contracts as $contract) {
                 $optionValue = $contract->NOMCONTRAT;
                 echo '<div class="contract-div">';
@@ -21,7 +21,7 @@
                 echo '<p>Type de contrat:' . $optionValue . '</p>';
                 echo '<p>Date d\'ouverture:' . $contract->DATEOUVERTURECONTRAT . '</p>';
 
-                echo '<button class="button" type="button"style="margin-left:30%;" onclick="switchForms(\'' . $optionValue . '\', \'' . $contract->DATEOUVERTURECONTRAT . '\', \'' . ($contract->DATEFERMETURE ? $contract->DATEFERMETURE : 'Indéterminée') . '\', \'' . $contract->TARIFMENSUEL . '\')">
+                echo '<button class="button" type="button"style="margin-left:20%;" onclick="switchForms(\'' . $optionValue . '\', \'' . $contract->DATEOUVERTURECONTRAT . '\', \'' . ($contract->DATEFERMETURE ? $contract->DATEFERMETURE : 'Indéterminée') . '\', \'' . $contract->TARIFMENSUEL . '\')">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -33,7 +33,7 @@
             }
         }
         ?>
-    </div>
+    
 </form>
 
 <form class="contract-details-page" id="contract-page" style="display: none;" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
